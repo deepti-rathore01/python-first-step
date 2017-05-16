8ee07057a3b11067c2fba267fd235f4e1a342f08
deepti <deepti.rathore01@vvdn.in>
2017-05-16 18:25:04 +0530

    to know the data type

---
+++ type.py
@@ -0,0 +1,14 @@
+"""
+
+@ file  : type.py
+@ brief : Find the given input as string|integer|float using type() and str|int|float|bool
+@ authore: deepti
+@ date : 16-may-17
+ 
+
+"""
+a = input("enter the value for a \n")
+print "a is what type ",type(a)
+a = raw_input("enter the value for a \n")
+print "a is what type",type(a)
+
