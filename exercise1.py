0789d011fe6358e79cfbd0960db33ec68d4115ca
deepti-rathore01 <deepti.rathore01@gmail.com>
2017-05-16 15:38:47 +0530

    Add files via upload

---
+++ exercise1.py
@@ -0,0 +1,18 @@
+# File     : program1.py
+# Brief    :A program for simple arithmetic operations like add, subtract, multiplication, division and etc
+# Authore  : Deepti
+# Date     : 16-may-17
+a = input("enter a number for a \n")
+b = input("enter a number for b  \n")
+
+choice = raw_input( " enter the operation you want  \n")
+
+if choice == 'add':print a+b
+if choice == 'sub':print a-b
+if choice == 'div':print a/b
+if choice == 'multiplication':print a*b
+if choice == 'mod':print a%b
+if choice == 'exponent':print a**b
+
+
+
