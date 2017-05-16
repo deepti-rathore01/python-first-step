fde65358510a9a7bde910dd83a282bff56ff6d57
deepti <deepti.rathore01@vvdn.in>
2017-05-16 16:12:33 +0530

    initial commit

---
+++ opertion1.py
@@ -0,0 +1,18 @@
+# File  : opertion1.py
+# berif : A program on all the opertion like add ,sub etc...
+# Authore : deepti
+# Date    : 16-may-17
+
+
+a = input ("enter a value for a \n")
+b = input ("enter a value for b\n")
+add = a+b
+print add
+sub = a-b
+print sub
+div = a/b
+print div
+multiply = a*b
+print multiply
+
+
