14a8017c2269a8ce34e99bc6519d8f494bc355c0
deepti <deepti.rathore01@vvdn.in>
2017-05-18 18:27:53 +0530

    new_word

---
+++ chapter-3/src/new_string.py
@@ -0,0 +1,13 @@
+"""
+@file   : new_string.py
+@authore : deepti
+@brief : the program to replace the odd position string with 'X' on any strings.
+@date : 18-5-17
+"""
+
+list = [ 'a', 'b','c','d']
+print "Value available at index 2 : "
+print list[2]
+list[2] = 'x'
+print "New value available at index 2 : "
+print list[2]
