15f8a96adc7e4ae90224f213169e4eb3bafc3095
deepti <deepti.rathore01@vvdn.in>
2017-05-19 18:10:27 +0530

    convert list into string

---
+++ chapter-3/src/converting_list_into_string.py
@@ -0,0 +1,11 @@
+"""
+@file   : converting_list_into_string
+@brief  :the program to convert all the elements of list into string
+@authore : deepti
+@date : 19-5-17
+"""
+alist = [12,'a',13,1, 'test']
+for i in range(5):
+    alist[i] =str(i)
+print alist
+    
