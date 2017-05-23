19485b1afedd3b41bffe9c0364730a6358cddeb5
deepti <deepti.rathore01@vvdn.in>
2017-05-23 09:46:35 +0530

    tuple commit

---
+++ chapter-3/src/tuple.py
@@ -0,0 +1,8 @@
+""""
+@file  : tuple.py
+@brief :Create one tuple and print the elements of tuple.
+"""
+
+tup1 = ('physics', 'chemistry', 1997, 2000);
+print" tuple",tup1
+print "tup1[0]: ", tup1[0]
