d05cd3280a64335e84b2ac243a7eca2f1a0bd68d
deepti <deepti.rathore01@vvdn.in>
2017-05-26 10:57:35 +0530

    chapter5 commit

--- chapter5/src/count_line.py
+++ chapter5/src/count_line.py
@@ -2,10 +2,15 @@
 @file   :count_line.py
 @brief  :Write a program to count no of lines in a text file.
 """
+def count_line(fname):
+
+    num_lines = 0
+    with open(fname, 'r') as f:
+        for line in f:
+            num_lines += 1
+    return num_lines
+
 fname = raw_input("Enter file name: ")
-num_lines = 0
-with open(fname, 'r') as f:
-    for line in f:
-         num_lines += 1
+num_lines = count_line(fname)
 print("Number of lines:")
 print(num_lines)
