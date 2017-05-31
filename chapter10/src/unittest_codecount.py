7de9adce586d6b505d4d7804f8b32e813348f3c4
deepti <deepti.rathore01@vvdn.in>
2017-05-31 14:47:05 +0530

    unitteat for code count

--- chapter10/src/unittest_codecount.py
+++ chapter10/src/unittest_codecount.py
@@ -5,13 +5,16 @@
 import unittest
 from code_count import*
 class count_code(unittest.TestCase):
- def test_pass(self):
-        self.assertTrue(True) 
-        
- def test_fail(self):
-        self.assertFalse(False)
+     def test_case1(self):
+         sub_string = my_sub_string_count(string ="code")
+         self.assertEqual(sub_string,1)  
+     def test_case2(self):
+         sub_string = my_sub_string_count(string = "cozecore") 
+         self.assertEqual(sub_string,2)
+     def test_case3(self):  
+         sub_string = my_sub_string_count(string ="12345$#")
+         self.assertEqual(sub_string,0)
 if __name__=="__main__":
-     unittest.main()
-
+       unittest.main()
 
 
