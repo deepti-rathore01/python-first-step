18dca4f5b349556d8acd2d07c01f002559277379
deepti <deepti.rathore01@vvdn.in>
2017-05-31 15:10:00 +0530

    import file for unittest for double char

---
+++ chapter10/src/double_char.py
@@ -0,0 +1,13 @@
+"""
+@file   :double_character
+@brief  :Given a string, return a string where for every char in the original, t         here are two chars.
+         Eg.   double_char('The') = 'TThhee'
+"""
+a = raw_input("give a string for doubling the character")
+
+def fun(a):
+    print " doubling the string character"
+    return "".join([x*2 for x in a])
+
+print fun(a)
+
